174def152b1fef13738cd8fd5c4e74dac9410fce
Victor Marinho <victorcmarinho@gmail.com>
2022-04-30 17:37:53 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Babel-Webpack
+
 Estudos de Babel + Webpack
+
+# Commandos
+
+```bash
+yarn babel src/index.jsx --out-file dist/bundle.js
+```
+
+```bash
+yarn webpack
+```
+
+```bash
+yarn webpack serve
+```
+
+```bash
+yarn babel src/index.jsx
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path"); // Tradutor de caminho para o sistema operacional
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  mode: process.env.NODE_ENV, // Modo de compilação development ou production
+  devtool: isDevelopment ? "eval-source-map" : "source-map", // Mapa de código original para debugger
+  entry: path.resolve(__dirname, "src", "index.jsx"), // Arquivo principal para ser compiladp
+  output: {
+    path: path.resolve(__dirname, "dist"), // Resultado da compilação
+    filename: "bundle.js", // Nome do arquivo compilado
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Extensões de arquivos que iram ser compilado
+  },
+  devServer: {
+    // Live reload configuration webpack-dev-serve
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8080,
+    hot: true, // Hot reload
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(), // Hot reload
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"), // Injeção do script no arquivo HTML principal
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        }, // Utilização do babel para compilar código Javascript para o navegador
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"], // Loader de CSS e SASS
+      },
+    ],
+  },
+};
